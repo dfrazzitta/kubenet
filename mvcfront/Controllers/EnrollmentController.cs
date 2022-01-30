bb6ec6719d99c7eb0393e285113eb21f79ad83b6
dfrazzitta <dfrazzitta@pctrax.com>
2022-01-30 08:23:28 -0500

    first

---
+++ mvcfront/Controllers/EnrollmentController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+ 
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mvcfront.Data;
+using mvcfront.Models;
+ using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvcfront.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+
+        private readonly SchoolContext _context;
+
+        public EnrollmentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+ 
+        // GET: Enrollment
+        public ActionResult Index()
+        {
+            var enrollments = _context.Enrollments.Include(e => e.Course).Include(e => e.Student);
+            return View(enrollments.ToList());
+        }
+
+        // GET: Enrollment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Enrollment enrollment = _context.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return NotFound(); //HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseID = new SelectList(_context.Courses, "CourseID", "Title");
+            ViewBag.StudentID = new SelectList(_context.Students, "ID", "LastName");
+            return View();
+        }
+
+        // POST: Enrollment/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598. [Bind("EnrollmentDate,FirstMidName,LastName")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("EnrollmentID, CourseID, StudentID, Grade")] Enrollment enrollment)
+        //public ActionResult Create([Bind("EnrollmentID,CourseID,StudentID,Grade")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Enrollments.Add(enrollment);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseID = new SelectList(_context.Courses, "CourseID", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(_context.Students, "ID", "LastName", enrollment.StudentID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Enrollment enrollment = _context.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+            ViewBag.CourseID = new SelectList(_context.Courses, "CourseID", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(_context.Students, "ID", "LastName", enrollment.StudentID);
+            return View(enrollment);
+        }
+ 
+
+        // POST: Enrollment/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(enrollment).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseID = new SelectList(_context.Courses, "CourseID", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(_context.Students, "ID", "LastName", enrollment.StudentID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Enrollment enrollment = _context.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = _context.Enrollments.Find(id);
+            _context.Enrollments.Remove(enrollment);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ mvcfront/Controllers/HomeController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvcfront.Models;
+using Microsoft.EntityFrameworkCore;
+using mvcfront.Data;
+using mvcfront.Models.SchoolViewModels;
+using System.Data.Common;
+using Microsoft.Extensions.Logging;
+
+
+
+namespace mvcfront.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+/*
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        using (var client1 = new System.Net.Http.HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                // request.RequestUri = new Uri("http://kapi/WeatherForecast/saveme"); // ASP.NET 3 (VS 2019 only)
+                string uriApi = "http://faApi/kubernetessystem/" + KubeType;
+                request.RequestUri = new Uri(uriApi); // ASP.NET 3 (VS 2019 only)
+
+                // request.RequestUri = new Uri("http://kapi/api/KubernetesSystem/GetSystemData"); // ASP.NET 3 (VS 2019 only)
+                //request.RequestUri = new Uri("http://kapi/api/WeatherForecast/"); // ASP.NET 2.x
+                var response = await client1.SendAsync(request);
+                var resp = await response.Content.ReadAsStringAsync();
+                return resp;
+            }
+*/
+  
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context, ILogger<HomeController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<ContentResult> CallApi()
+        {
+
+            using (var client1 = new System.Net.Http.HttpClient())
+            {
+                
+                // Call *mywebapi*, and display its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                // requcd ..est.RequestUri = new Uri("http://kapi/WeatherForecast/saveme"); // ASP.NET 3 (VS 2019 only)
+                //string uriApi = "http://localhost:5001/weatherforecast/";
+                string uriApi = "http://apiback/weatherforecast/";
+                request.RequestUri = new Uri(uriApi); // ASP.NET 3 (VS 2019 only)
+
+                // request.RequestUri = new Uri("http://kapi/api/KubernetesSystem/GetSystemData"); // ASP.NET 3 (VS 2019 only)
+                //request.RequestUri = new Uri("http://kapi/api/WeatherForecast/"); // ASP.NET 2.x
+                var response = await client1.SendAsync(request);
+                var resp = await response.Content.ReadAsStringAsync();
+                return Content(resp); //View(resp);
+
+            }
+           // return View();
+        }
+
+
+
+        public IActionResult Index()
+        {
+            _logger.LogDebug("enter Index");
+            return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            List<EnrollmentDateGroup> groups = new List<EnrollmentDateGroup>();
+            var conn = _context.Database.GetDbConnection();
+            try
+            {
+                await conn.OpenAsync();
+                using (var command = conn.CreateCommand())
+                {
+                    string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
+                        + "FROM Student "
+                       // + "WHERE Discriminator = 'Student' "
+                        + "GROUP BY EnrollmentDate";
+                    command.CommandText = query;
+                    DbDataReader reader = await command.ExecuteReaderAsync();
+
+                    if (reader.HasRows)
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var row = new EnrollmentDateGroup { EnrollmentDate = reader.GetDateTime(0), StudentCount = reader.GetInt32(1) };
+                            groups.Add(row);
+                        }
+                    }
+                    reader.Dispose();
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return View(groups);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+       
+
+    }
+}
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mvcfront.Models;
+
+namespace mvcfront.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+*/
